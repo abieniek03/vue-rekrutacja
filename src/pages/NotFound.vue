2b0ba09060fbd220bee42fe227ab376374e7127e
Adrian Bieniek <kontakt@abieniek.dev>
2024-02-24 15:56:43 +0100

    Add 404 page

---
+++ src/pages/NotFound.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts"></script>
+
+<template>
+	<section class="not-found">
+		<div class="not-found__container">
+			<h1 class="not-found__title">Nie znaleziono takiej strony</h1>
+			<p class="not-found__code">404</p>
+			<router-link to="/" class="router-link">wróć na stronę główną</router-link>
+		</div>
+	</section>
+</template>
+
+<style scoped lang="scss">
+.not-found {
+	&__container {
+		text-align: center;
+	}
+	&__title {
+		font-size: 2rem;
+	}
+	&__code {
+		margin-bottom: 1rem;
+		font-size: 5rem;
+		font-weight: bold;
+	}
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -3,10 +3,13 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "../pages/Home.vue";
 import Post from "../pages/Post.vue";
 
+import NotFound from "../pages/NotFound.vue";
+
 const routes = [
 	{ path: "/", component: Home },
 	{ path: "/:id", component: Post },
-	
+	{ path: "/404", component: NotFound },
+	{ path: "/:pathMatch(.*)", redirect: "/404" },
 ];
 
 const router = createRouter({
